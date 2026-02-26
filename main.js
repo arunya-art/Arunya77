32685f30e835d8f9b0a71b866267b9451ac46904
arunya-art <satnamsinghama@gmail.com>
2026-02-26 21:22:20 +0530

    Update main.js

--- main.js
+++ main.js
@@ -489,13 +489,4 @@ submitForm(
 );
 document.addEventListener("DOMContentLoaded", function () {
 
-  const hamburger = document.getElementById("hamburger");
-  const navLinks = document.getElementById("navLinks");
-
-  if (hamburger && navLinks) {
-    hamburger.addEventListener("click", () => {
-      navLinks.classList.toggle("active");
-    });
-  }
-
-});
+ 
